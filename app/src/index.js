85942dac78a01622e82ede224cfee37c107925ad
Ryan Temple <temple.design.me@gmail.com>
2018-01-12 17:04:35 -0500

    initial commit

---
+++ app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/app/app';
+
+import './index.css';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+registerServiceWorker();
